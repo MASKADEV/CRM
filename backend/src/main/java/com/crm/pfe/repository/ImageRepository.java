9f68c851a83a4473443c17e5383c5220f22f3c26
MASKADEV <sidkiyouness@gmail.com>
2024-05-30 15:22:39 +0000

    refactoring project pattern

--- backend/src/main/java/com/crm/pfe/repository/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.crm.pfe.repository;
-
-import com.crm.pfe.model.ProductImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<ProductImage, Integer> {
-}
--- backend/src/main/java/com/crm/pfe/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.crm.pfe.repository;
-
-import com.crm.pfe.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ProductRepository extends JpaRepository<Product, UUID> {
-}
